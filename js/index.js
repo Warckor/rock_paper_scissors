a396c1870608d295c2adbe2199f1a4c75480d847
u029299 <miguel.gorostola.ext@rsi.cajarural.com>
2023-08-30 08:49:49 +0200

    Add javascript logic

--- js/index.js
+++ js/index.js
@@ -1,49 +1,75 @@
-function getComputerChoice() {
-    const choices = ['rock', 'paper', 'scissors'];
-    const randomNumber = Math.floor(Math.random() * choices.length);
-    return choices[randomNumber];
-}
+const playerScoreSpan = document.getElementById('player-score');
+const computerScoreSpan = document.getElementById('computer-score');
+const matchResult = document.querySelector('.match-result > h3');
+const selectionButtons = document.querySelectorAll('.buttons > div > input');
+let playerScore = 0;
+let computerScore = 0;
 
-function playRound(playerSelection, computerSelection) {
+/**
+ * The function `playRound` determines the winner of a rock-paper-scissors game between a player and a
+ * computer.
+ * @param playerSelection - The player's choice for the round (rock, paper, or scissors).
+ * @param computerSelection - The computer's selection in the game (rock, paper, or scissors).
+ */
+const playRound = (playerSelection, computerSelection) => {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
     if (playerSelection === computerSelection) {
-        return 'It\'s a tie!';
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return 'You win! Rock beats scissors!';
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return 'You lose! Paper beats rock!';
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return 'You win! Paper beats rock!';
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return 'You lose! Scissors beats paper!';
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return 'You win! Scissors beats paper!';
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return 'You lose! Rock beats scissors!';
+        matchResult.textContent = 'It\'s a tie!';
     }
-}
-
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt('Rock, Paper, or Scissors?');
-        const computerSelection = getComputerChoice();
-        const result = playRound(playerSelection, computerSelection);
-        console.log(result);
-        if (result.includes('win')) {
-            playerScore++;
-        } else {
-            computerScore++;
-        }
+    else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+        matchResult.textContent = 'You win!';
+        playerScore++;
+    }
+    else {
+        matchResult.textContent = 'You lose!';
+        computerScore++;
     }
 
-    if (playerScore > computerScore) {
-        console.log('You win!');
-    } else if (playerScore < computerScore) {
-        console.log('You lose!');
-    } else {
-        console.log('It\'s a tie!');
-    }
+    selectionEffects(matchResult);
+
+    playerScoreSpan.textContent = playerScore;
+    computerScoreSpan.textContent = computerScore;
 }
+
+/**
+ * The function `getComputerChoice` returns a random choice of 'rock', 'paper', or 'scissors'.
+ * @returns The function `getComputerChoice` returns a randomly selected choice from the array
+ * `choices`, which can be either 'rock', 'paper', or 'scissors'.
+ */
+const getComputerChoice = () => {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+const selectionEffects = (e) => {
+    e.classList.add('selection');
+    setTimeout(() => {
+        e.classList.remove('selection');
+    }, 300);
+}
+
+const resultEffects = (e) => {
+    e.classList.add('result-effect');
+    setTimeout(() => {
+        e.classList.remove('result-effect');
+    }, 500);
+}
+
+/* The code `selectionButtons.forEach((button) => { ... })` is adding an event listener to each button
+in the `selectionButtons` array. */
+selectionButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log(button);
+        selectionEffects(button);
+        playerSelection = button.value;
+        computerSelection = getComputerChoice();
+
+        playRound(playerSelection, computerSelection);
+    })
+})
